967d84076ebead6220fcc70332cf56445cd34587
Cyrine-belhassen <73298838+Cyrine-belhassen@users.noreply.github.com>
2022-02-24 10:42:25 +0100

    entity forum

---
+++ src/main/java/tn/sirine/spring/WomanAreaApplication.java
@@ -0,0 +1,13 @@
+package tn.sirine.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WomanAreaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WomanAreaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/tn/sirine/spring/entities/Like.java
@@ -0,0 +1,40 @@
+package tn.sirine.spring.entities;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Like {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private int countLike;
+	private Timestamp likeDate;
+	
+	@ManyToOne
+	private Comment comment;
+	
+	@OneToOne
+	private User userl;
+}
