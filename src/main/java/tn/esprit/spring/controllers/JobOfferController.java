5711d9d2b6efec1add83b6d1c27b0fa7ded24fa9
Cyrine-belhassen <73298838+Cyrine-belhassen@users.noreply.github.com>
2022-02-28 10:59:46 +0100

    edit entity

---
+++ src/main/java/tn/esprit/spring/controllers/JobOfferController.java
@@ -0,0 +1,41 @@
+package tn.esprit.spring.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import tn.esprit.spring.entities.JobOffer;
+import tn.esprit.spring.services.IJobOfferService;
+
+@RestController
+public class JobOfferController {
+
+	@Autowired
+	IJobOfferService js ;
+	@PostMapping("/addOffer")
+	public JobOffer addOffer(@RequestBody JobOffer j){
+		return js.addOffer(j);
+	}
+	@DeleteMapping("/deleteOffer/{idOffer}")
+	public void deleteOffer(@PathVariable long idOffer){
+		js.deleteOffer(idOffer);
+	}
+	@PutMapping("/updateOffer")
+	public JobOffer updateOffer(@RequestBody JobOffer j){
+		return js.updateJobOffer(j);
+		}
+	@GetMapping("/getOneOffer/{idOffer}")
+	public JobOffer showOffer(@PathVariable long idOffer ){
+		return js.showOneOffer(idOffer);	
+	}
+	@GetMapping("/ShowAllOffer")
+	public List<JobOffer> showAllOffer(){	
+		return js.showAllOffer();			
+	}
+}
---
+++ src/main/java/tn/esprit/spring/services/IInterviewService.java
@@ -0,0 +1,19 @@
+package tn.esprit.spring.services;
+
+import java.util.List;
+
+import tn.esprit.spring.entities.Interview;
+
+public interface IInterviewService {
+
+	 Interview addInterview(Interview i);
+	 //void addlisInterviews (List<Interview> iv ,Long userId, Long idOffer); 
+	 Interview showInterview(Long idInterview);
+	 List<Interview> showAllInterview();
+	 // List<Interview> listInterviewsParUser (Long userId);
+	 //List<Interview> listInterviewsParOfferAndUser ( Long idOffer,Long userId );
+	 Interview UpdateInterview (Interview i);
+	 void deleteInterview (Long idInterview);
+
+	 
+}
