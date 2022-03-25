0c96406c0a2204e76db1e86ee5cec92f8d3ae4f6
Ramzi Kerkeni <ramzi.kerkeni@esprit.tn>
2022-03-25 20:53:24 +0100

    Working User part

--- src/main/java/tn/esprit/spring/womanarea/demo/DemoApplication.java
+++ src/main/java/tn/esprit/spring/womanarea/demo/DemoApplication.java
@@ -4,7 +4,6 @@ import com.twilio.Twilio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import tn.esprit.spring.womanarea.demo.Configurations.TwilioConfig;
 
 import javax.annotation.PostConstruct;
