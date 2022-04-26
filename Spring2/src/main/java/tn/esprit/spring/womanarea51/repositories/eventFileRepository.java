6d1b39006d519c64770db79f9d7cad1bf5b5fae8
Azzabinada <nada.azzabi@esprit.tn>
2022-04-26 18:42:05 +0100

    fixed fund file upload

--- Spring2/src/main/java/tn/esprit/spring/womanarea51/repositories/eventFileRepository.java
+++ Spring2/src/main/java/tn/esprit/spring/womanarea51/repositories/eventFileRepository.java
@@ -1,9 +1,10 @@
 package tn.esprit.spring.womanarea51.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import tn.esprit.spring.womanarea51.entities.eventFile;
-
+@Repository
 public interface eventFileRepository extends JpaRepository<eventFile,Long>{
 
 }
