dbafae44a85a8b4d5028f287df47fed1fd5041be
Ramzi Kerkeni <ramzi.kerkeni@esprit.tn>
2022-04-26 07:28:46 +0100

    Allow admin to append a course to a specific instructor

--- Spring2/src/main/java/tn/esprit/spring/womanarea51/services/CourseServiceImpl.java
+++ Spring2/src/main/java/tn/esprit/spring/womanarea51/services/CourseServiceImpl.java
@@ -2,11 +2,9 @@ package tn.esprit.spring.womanarea51.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import tn.esprit.spring.womanarea51.entities.Course;
-import tn.esprit.spring.womanarea51.entities.CourseCategory;
-import tn.esprit.spring.womanarea51.entities.ERole;
-import tn.esprit.spring.womanarea51.entities.User;
+import tn.esprit.spring.womanarea51.entities.*;
 import tn.esprit.spring.womanarea51.repositories.CourseRepository;
+import tn.esprit.spring.womanarea51.repositories.RoleRepository;
 import tn.esprit.spring.womanarea51.repositories.UserRepository;
 
 import java.util.List;
@@ -18,9 +16,13 @@ public class CourseServiceImpl implements CourseService{
     @Autowired
     UserRepository ur;
 
+    @Autowired
+    RoleRepository rr;
+
     @Override
     public Course add_course(Course c, User U) {
-        c.setInstructor(U);
+        if (U.getRoles().contains(rr.findByName(ERole.ROLE_ADMIN).orElse(null))) c.setInstructor(ur.findByUsername(c.getInstructor().getUsername()).orElse(U));
+        else c.setInstructor(U);
         return cr.save(c);
     }
 
@@ -28,7 +30,7 @@ public class CourseServiceImpl implements CourseService{
     public Course update_course(Course c, User U) {
         Course old_C = cr.findById(c.getCourseId()).orElse(null);
         if (old_C == null) return null;
-        if (U.getUsername().equals(old_C.getInstructor().getUsername()) || U.getRoles().contains(ERole.ROLE_ADMIN)) {
+        if (U.getUsername().equals(old_C.getInstructor().getUsername()) || U.getRoles().contains(rr.findByName(ERole.ROLE_ADMIN).orElse(null))) {
             c.setQuiz(old_C.getQuiz());
             c.setFiles(old_C.getFiles());
             c.setInstructor(old_C.getInstructor());
@@ -46,7 +48,7 @@ public class CourseServiceImpl implements CourseService{
     public void delete_course(Long idCourse, User u) {
         Course old_course = cr.findById(idCourse).orElse(null);
         if (old_course != null) {
-            if (u.getRoles().contains(ERole.ROLE_ADMIN)) cr.delete(old_course);
+            if (u.getRoles().contains(rr.findByName(ERole.ROLE_ADMIN).orElse(null))) cr.delete(old_course);
             else {
                 if (old_course.getInstructor().getUsername().equals(u.getUsername())){
                     cr.delete(old_course);
