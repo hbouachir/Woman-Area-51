675d3e6c15ae07f9b51bd90ca1d1cf4df5fa53ca
Ramzi Kerkeni <ramzi.kerkeni@esprit.tn>
2022-05-10 00:11:21 +0100

    fix Hamza Enrollement

--- Spring2/src/main/java/tn/esprit/spring/womanarea51/controllers/EnrollementController.java
+++ Spring2/src/main/java/tn/esprit/spring/womanarea51/controllers/EnrollementController.java
@@ -24,7 +24,7 @@ import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600,allowCredentials = "true")
 @RestController
-
+@RequestMapping("/api")
 public class EnrollementController {
 
     @Autowired
